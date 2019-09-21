56dae448e7f75771c7536e9309b16833627d4096
allequalit <50491988+allequalit@users.noreply.github.com>
2019-09-21 13:51:08 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
+# 업데이트 목록 갱신
+apt-get update
+
+# 우분투 iptables 서비스 활성화
+# insserv: warning: script ‘K01iptables’ missing LSB tags and overrides
+# insserv: warning: script ‘iptables’ missing LSB tags and overrides
+# 위와 같은 애러가 표시될 경우 게시글(https://idchowto.com/?p=31482)을 참조하여 문제를 해결해주세요.
+
+apt-get install iptables-persistent -y
+cp /usr/share/netfilter-persistent/plugins.d/15-ip4tables /etc/init.d/iptables
+/etc/init.d/iptables start
+/etc/init.d/iptables flush
+update-rc.d -f iptables defaults
+
 # 80, 443 포트 클라우드플레어 아이피주소를 제외한 모든 아이피 주소 차단
 
 iptables -A INPUT -p tcp --dport 80 -j DROP
@@ -51,9 +65,16 @@ ip6tables -A INPUT -p tcp --dport 443 -s 2405:8100::/32 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 443 -s 2a06:98c0::/29 -j ACCEPT 
 ip6tables -A INPUT -p tcp --dport 443 -s 2c0f:f248::/32 -j ACCEPT 
 
+wget https://cl.ael.kr/rules.v4
+cat rules.v4 > /etc/iptables/rules.v4
+
+wget https://cl.ael.kr/rules.v6
+cat rules.v6 > /etc/iptables/rules.v6
+
 # IP 스푸핑 방지를 활성화 하고 소스 주소 확인을 켭니다.
 
 sysctl  -w net.ipv4.conf.all.rp_filter=1
 
 wget https://cl.ael.kr/sysctl.conf
 cat sysctl.conf > /etc/sysctl.conf
+ 
